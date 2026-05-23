ec67ef8e548b4e5f37a9b3914e8942e0a656eabf
AlexeyFrens <leiloka46@gmail.com>
2026-05-22 23:57:07 -0300

    First commit

---
+++ Compy/CompyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompyApp.swift
+//  Compy
+//
+//  Created by João Cláudio dos Santos Souza on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CompyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Compy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Compy
+//
+//  Created by João Cláudio dos Santos Souza on 22/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
