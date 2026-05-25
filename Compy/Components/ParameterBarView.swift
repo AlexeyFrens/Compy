2ad728aa28c3ebf2c18d578271f921e8649e9e71
André Holovati <andreholovati@gmail.com>
2026-05-25 15:23:09 -0300

    feat: Adição das models para criação da barra de tarefas

---
+++ Compy/Components/ParameterBarView.swift
@@ -0,0 +1,15 @@
+//
+//  ParameterBarView.swift
+//  Compy
+//
+//  Created by Andre on 25/05/26.
+//
+import SwiftUI
+struct ParameterBarView: View{
+    var barra : ParameterBarViewModel
+    var body: some View{
+        HStack{
+            
+        }
+    }
+}
